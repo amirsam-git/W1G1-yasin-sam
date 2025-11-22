b1241bc56a5f5a4d5b585f0bafb8b47e261b64a0
sali mft <thesalimft@gmail.com>
2025-11-22 17:41:52 +0330

    add second

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,25 +1,40 @@
+import { useState, useEffect } from "react";
+
 export default function ProductCard() {
-  return (
-    <div className="product-card">
-        <div>
-            <div className="d-flex item-start">
-                <div className="product-card-img-area">
-                    <img src="/assets/img/phones/mobile1.webp" alt="" />
-                    <p>special sale</p>
-                    <p>35:36:05</p>
-                </div>
-                <div>
-                    <p className="product-card-title">iPhone 15 black 256 2025</p>
-                    <p>blue</p>
-                    <p>Digikala</p>
-                    <p>
-                        <i class="bi bi-mailbox2"></i>
-                        <span>Post package</span>
-                    </p>
-                    <p>Europ, US, UK</p>
+
+    const [seconds, setSeconds] = useState(59);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds((prev) => {
+                if (prev === 0) return 59; // restart or you can stop it
+                return prev - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="product-card">
+            <div>
+                <div className="d-flex item-start">
+                    <div className="product-card-img-area">
+                        <img src="/assets/img/phones/mobile1.webp" alt="" />
+                        <p>special sale</p>
+                        <p>5:36: {seconds}</p>
+                    </div>
+                    <div>
+                        <p className="product-card-title">iPhone 15 black 256 2025</p>
+                        <p>blue</p>
+                        <p>Digikala</p>
+                        <p>
+                            <i class="bi bi-mailbox2"></i>
+                            <span>Post package</span>
+                        </p>
+                        <p>Europ, US, UK</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
