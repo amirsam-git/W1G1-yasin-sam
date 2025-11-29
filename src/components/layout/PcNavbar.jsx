7f141b66d6a4bfef51ac02283eaa1d0cd9604c19
sali mft <thesalimft@gmail.com>
2025-11-29 20:34:28 +0330

    add topbar

--- src/components/layout/PcNavbar.jsx
+++ src/components/layout/PcNavbar.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 function PcNavbar() {
   return (
-    <>
+    <div className='topbar d-flex justify-content-between item-center'>
+      <div className=''>
+        <img src="/vite.svg" alt="blog" />
+      </div>
       <div className="top-navbar-area d-lg-blcok d-sm-none w-99">
         <div>
           <Link to="/store">Store</Link>
@@ -21,7 +24,10 @@ function PcNavbar() {
           <Link to="/contact-us">Contact Us</Link>
         </div>
       </div>
-    </>
+      <div className='me-4'>
+        <Link to="/login" className='btn btn-warning' style={{color:'black'}}>Login</Link>
+      </div>
+    </div>
   )
 }
 
