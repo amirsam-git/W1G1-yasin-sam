bbd46d3e5254a4ac27c4ecf8d71931b845036938
amirsam-git <amirsamsohrabi98@gmail.com>
2025-12-24 21:45:03 +0330

    background-color

--- src/components/InfluencerArea.jsx
+++ src/components/InfluencerArea.jsx
@@ -1,10 +1,10 @@
 function InfluencerArea(){
     return (
         <div className="influencer-area d-flex">
-            <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
-            <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
-            <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
-            <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
+            <div><img src="/assets/img/key-1.png" alt="" /></div>
+            <div><img src="/assets/img/key-1.png" alt="" /></div>
+            <div><img src="/assets/img/key-1.png" alt="" /></div>
+            <div><img src="/assets/img/key-1.png" alt="" /></div>
             <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
             <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
             <div><img src="/assets/img/influencer01.jpg" alt="" /></div>
