0703b16080f34e1aa73a74d5ea0da64bd980f136
amirsam-git <amirsamsohrabi98@gmail.com>
2025-12-26 10:24:32 +0330

    offers

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ export default function ProductCard() {
     useEffect(() => {
         const interval = setInterval(() => {
             setSeconds((prev) => {
-                if (prev === 0) return 59; // restart or you can stop it
+                 if (prev === 0) return 59;  //restart or you can stop it
                 return prev - 1;
             });
         }, 1000);
@@ -15,61 +15,96 @@ export default function ProductCard() {
     }, []);
 
     return (
-        <div className="product-card">
-            <div>
-                <div className="d-flex item-start">
-                    <div className="product-card-img-area">
-                        <img src="/assets/img/offer/demon crown.png" alt="" />
-                        <p>special sale</p>
-                        <p>5:36: {seconds}</p>
-                    </div>
-                    <div>
-                        <p className="product-card-title product-card-title-demon">Demon rank</p>
-                        <p>30d</p>
-                        <p>
-                            <i class="bi bi-hammer"></i>
-                            <span>SkyMine</span>
-                        </p>
-                        <p>Fly,Specte,Glow,...</p>
-                    </div>
+
+        <div>
 
 
-                    <div className="d-flex item-start">
-                        <div className="product-card-img-area">
-                            <img src="/assets/img/offer/angel crown.png" alt="" />
-                            <p>special sale</p>
-                            <p>5:36: {seconds}</p>
-                        </div>
-                        <div>
-                            <p className="product-card-title product-card-title-angel">Angel rank</p>
-                            <p>30d</p>
-                            <p>
-                                <i class="bi bi-hammer"></i>
-                                <span>SkyMine</span>
-                            </p>
-                            <p>Fly,Glow,...</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="d-flex item-start">
+            
+            
+        <div className="product-card rank-row">
+            <div className="item-start">
                 <div className="product-card-img-area">
-                    <img src="/assets/img/offer/nature crown.png" alt="" />
+                    <img src="/assets/img/offer/demon crown.png" alt="" />
                     <p>special sale</p>
                     <p>5:36: {seconds}</p>
                 </div>
                 <div>
-                    <p className="product-card-title product-card-title-nature">Nature rank</p>
+                    <p className="rank-card product-card-title product-card-title-demon">Demon rank</p>
                     <p>30d</p>
                     <p>
                         <i class="bi bi-hammer"></i>
                         <span>SkyMine</span>
                     </p>
-                    <p>Glow,..</p>
+                    <p>Fly,Specte,Glow,...</p>
                 </div>
             </div>
+
+            <div className="item-start">
+                <div className="product-card-img-area">
+                    <img src="/assets/img/offer/angel crown.png" alt="" />
+                    <p>special sale</p>
+                    <p>5:36: {seconds}</p>
+                </div>
+                <div>
+                    <p className="rank-card product-card-title product-card-title-angel">Angel rank</p>
+                    <p>30d</p>
+                    <p>
+                        <i class="bi bi-hammer"></i>
+                        <span>SkyMine</span>
+                    </p>
+                    <p>Fly,Glow,...</p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+                
+        <div className="product-card rank-row">
+                <div className="item-start">
+                     <div className="product-card-img-area">
+                       <img src="/assets/img/offer/nature crown.png" alt="" />
+                         <p>special sale</p>
+                         <p>5:36: {seconds}</p>
+                     </div>
+                     <div>
+                         <p className="rank-card product-card-title product-card-title-nature">Nature rank</p>
+                         <p>30d</p>
+                         <p>
+                             <i class="bi bi-hammer"></i>
+                             <span>SkyMine</span>
+                         </p>
+                         <p>Glow,..</p>
+                     </div>
+                 </div>
+    
+
+                 <div className="item-start">
+                     <div className="product-card-img-area">
+                         <img src="/assets/img/offer/sungod crown.png" alt="" />
+                         <p>special sale</p>
+                         <p>5:36: {seconds}</p>
+                     </div>
+                     <div>
+                         <p className="rank-card product-card-title product-card-title-sungod">SunGod rank</p>
+                         <p>30d</p>
+                         <p>
+                             <i class="bi bi-hammer"></i>
+                             <span>SkyMine</span>
+                         </p>
+                         <p>Glow,Ec,Fly,...</p>
+                     </div>
+                 </div>
+             </div>
+        
+
+
+
+
         </div>
     )
 }
